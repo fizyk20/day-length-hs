155963f7c7b858f401b097813cb3d7a3002c115c
Bartłomiej Kamiński <fizyk20@gmail.com>
2015-12-22 15:24:40 +0100

    Time zone support

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ import Control.Monad
 import Data.Time.Clock
 import Data.Time.Calendar
 import Data.Time.Format
+import Data.Time.LocalTime
 import Data.List
 import System.Locale
 import Options.Applicative
@@ -14,11 +15,11 @@ showInterval (Just t) = show h ++ "h " ++ show m ++ "min " ++ show s ++ "s"
           m = floor ((t - 3600 * fromIntegral h) / 60)
           s = floor (t - 3600 * fromIntegral h - 60 * fromIntegral m)
           
-showTime :: Maybe UTCTime -> String
+showTime :: (FormatTime t) => Maybe t -> String
 showTime Nothing = "N/A"
 showTime (Just x) = formatTime defaultTimeLocale "%T" x
 
-showDate :: Maybe UTCTime -> String
+showDate :: (FormatTime t) => Maybe t -> String
 showDate Nothing = "Nothing"
 showDate (Just x) = formatTime defaultTimeLocale "%F" x
 
@@ -35,7 +36,8 @@ data Options = Options {
     latitude :: Double,
     longitude :: Double,
     start :: Day,
-    end :: MDay
+    end :: MDay,
+    timeZone :: TimeZone
 }
 
 latitudeParser :: Parser Double
@@ -43,7 +45,7 @@ latitudeParser = option auto
      ( long "latitude"
     <> short 'l'
     <> metavar "LAT"
-    <> help "Location latitude"
+    <> help "Location latitude (defaults to 52.2)"
     <> value 52.2 )
     
 longitudeParser :: Parser Double
@@ -51,31 +53,40 @@ longitudeParser = option auto
      ( long "longitude"
     <> short 'g'
     <> metavar "LON"
-    <> help "Location longitude"
+    <> help "Location longitude (defaults to 20.9)"
     <> value 20.9 )
     
-startParser :: Parser Day
-startParser = option auto
+startParser :: Day -> Parser Day
+startParser defDay = option auto
      ( long "start"
     <> short 's'
     <> metavar "YYYY-MM-DD"
-    <> help "The starting date"
-    <> value (fromGregorian 2015 3 21) )
+    <> help "The starting date (defaults to current date)"
+    <> value defDay )
     
 endParser :: Parser MDay
 endParser = option auto
      ( long "end"
     <> short 'e'
     <> metavar "YYYY-MM-DD"
-    <> help "The ending date"
+    <> help "The ending date (defaults to the same as starting date)"
     <> value (MDay Nothing) )
+    
+timeZoneParser :: TimeZone -> Parser TimeZone
+timeZoneParser defTZ = option (auto >>= \x -> return (hoursToTimeZone x))
+     ( long "zone"
+    <> short 'z'
+    <> metavar "OFFSET"
+    <> help "Time zone offset from UTC in hours (defaults to local time zone)"
+    <> value defTZ )
 
-opts :: Parser Options   
-opts = Options
+opts :: Day -> TimeZone -> Parser Options   
+opts defDay defTZ= Options
     <$> latitudeParser
     <*> longitudeParser
-    <*> startParser
+    <*> startParser defDay
     <*> endParser
+    <*> timeZoneParser defTZ
     
 genDayList :: Day -> MDay -> [Day]
 genDayList x (MDay Nothing) = [x]
@@ -83,21 +94,27 @@ genDayList x my@(MDay (Just y))
     | x > y = []
     | otherwise = x : genDayList (addDays 1 x) my
     
-formatLine :: Location -> Day -> String
-formatLine l d = showDate (Just $ UTCTime d 0) ++ ": rise " ++ showTime rise ++ ", set " ++ showTime set ++ ", length = " ++ showInterval len
+formatLine :: TimeZone -> Location -> Day -> String
+formatLine tz l d = showDate date ++ ": rise " ++ showTime rise
+    ++ ", set " ++ showTime set ++ ", length = " ++ showInterval len
     where
-        rise = sunrise l d
-        set = sunset l d
+        date = Just $ LocalTime d midnight
+        rise = utcToLocalTime tz <$> sunrise l d
+        set = utcToLocalTime tz <$> sunset l d
         len = dayLength l d
 
 main = do
-    options <- execParser (info (helper <*> opts)
+    localTZ <- getCurrentTimeZone
+    curTime <- getCurrentTime
+    options <- execParser (info (helper <*> opts (utctDay curTime) localTZ)
          ( fullDesc
         <> progDesc "Calculate the length of the given day"
         <> header "Day length calculator" ))
+    let tz = timeZone options
     let list = genDayList (start options) (end options)
     let lat = latitude options
     let lon = longitude options
     putStrLn ("Location: Lat " ++ show lat ++ "; Lon " ++ show lon)
-    mapM_ (putStrLn . formatLine (Loc lat lon)) list
+    putStrLn ("Time zone offset: " ++ timeZoneOffsetString tz)
+    mapM_ (putStrLn . formatLine tz (Loc lat lon)) list
     
